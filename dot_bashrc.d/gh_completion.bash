8739eb25481c8fdd58a8d2b9fc762dd404091374
Kent Larsson <kent.larsson@gmail.com>
2025-02-15 18:33:27 +0100

    add gh classroom bash completion startup script

--- dot_bashrc.d/gh_completion.bash
+++ dot_bashrc.d/gh_completion.bash
@@ -2,4 +2,5 @@ if command -v gh &>/dev/null; then
     __BASHRC_SOURCE_COMMAND_OUTPUT gh completion -s bash
     __BASHRC_SOURCE_COMMAND_OUTPUT gh copilot alias bash
     __BASHRC_SOURCE_COMMAND_OUTPUT gh copilot completion bash
+    __BASHRC_SOURCE_COMMAND_OUTPUT gh classroom completion bash
 fi
