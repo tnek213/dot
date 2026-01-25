d59976cd4e6b3c201397854addefb4b15b0d5bcf
Kent Larsson <kent.larsson@gmail.com>
2026-01-25 10:20:06 +0100

    fix

--- dot_bashrc.d/00_java_home.bash
+++ dot_bashrc.d/00_java_home.bash
@@ -1,12 +1,12 @@
 # shellcheck disable=SC1091
 
-___JAVA_HOME=$(
+___BASHRC_JAVA_HOME=$(
   find "$HOME/.local/share/" -maxdepth 1 -type d -name 'jdk-*' \
     -exec test -x '{}/bin/javac' \; -print |
     sort -Vr | sed -n '1p'
 )
 
-if [ -n "$___JAVA_HOME" ]; then
-  export JAVA_HOME="$___JAVA_HOME"
+if [ -n "$___BASHRC_JAVA_HOME" ]; then
+  export JAVA_HOME="$___BASHRC_JAVA_HOME"
   export PATH="$JAVA_HOME/bin:$PATH"
 fi
--- dot_bashrc.d/10_gh.bash
+++ dot_bashrc.d/10_gh.bash
@@ -3,9 +3,9 @@ if command -v gh &>/dev/null; then
     __BASHRC_SOURCE_COMMAND_OUTPUT gh classroom completion bash
 
     # shellcheck disable=SC2046,SC2317,SC2329
-    gh-repo-create-TEMPL() {
+    ___BASHRC_GH_TEMPLATE() {
         if [ $# -gt 0 ]; then
-            cd "$1" || echo "Error: invalid target $1" >&2 && return 1
+            cd "$1" || { echo "Error: invalid target $1" >&2 && return 1; }
         fi
 
         gh repo create \
@@ -20,9 +20,9 @@ if command -v gh &>/dev/null; then
         git push -f
     }
 
-    for TEMPL in kc8se nackc8; do
-        eval "$(declare -f gh-repo-create-TEMPL | sed "s/TEMPL/$TEMPL/")"
+    for ___BASHRC_GH_ORG in kc8se nackc8; do
+        eval "$(declare -f ___BASHRC_GH_TEMPLATE | sed "s/TEMPL/$___BASHRC_GH_ORG/")"
     done
 
-    unset gh-repo-create-TEMPL
+    unset ___BASHRC_GH_TEMPLATE
 fi
